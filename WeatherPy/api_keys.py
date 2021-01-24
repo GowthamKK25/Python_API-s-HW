2fb3a2c52135874ba7109464daca96af83a36214
Gowtham <gowthukrishna1@gmail.com>
2021-01-24 14:55:14 -0800

    3

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "557bdb71d5f06e2d64d315e3d4a003f6"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+gkey = "AIzaSyD-SqC6rsWJKih9YsVLec_eVRhAX4S_xMk"
